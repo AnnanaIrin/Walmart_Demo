b18bf1ba2afdb69894005c2e0344f199e09a49ae
AnnanaIrin <msakther786@gmail.com>
2022-04-27 15:05:39 -0400

    new class

---
+++ src/home/Pc.java
@@ -0,0 +1,26 @@
+package home;
+
+public class Pc {
+ public int doSum(){
+     int num1=23;
+     int num2=13;
+     int total=num1+num2;
+     System.out.println("total"+total);
+    return total;
+ }
+
+    public static void main(String[] args) {
+
+        Pc ob=new Pc();
+        ob.doSum();
+
+
+    }
+
+
+
+
+
+
+
+}
